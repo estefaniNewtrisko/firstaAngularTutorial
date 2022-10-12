8ada305255d21ece6b2718ae5d791562f1330c83
estefaniNewtrisko <estefani.paredes@newtrisko.de>
2022-10-12 14:23:27 +0200

    clients app finished

---
+++ src/app/clientes/cliente.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Cliente } from './cliente';
+import { CLIENTES } from "./clientes.json";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  constructor() { }
+
+  getClientes(): Observable<Cliente[]> {
+    return of(CLIENTES); //convert Observable
+  }
+}
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { ClienteService } from "./cliente.service";
 
 @Component({
   selector: 'app-clientes',
@@ -8,23 +9,15 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[]=[
-    {id:1, nombre:"Estefani", apellido:"Paredes Valera", email:"estefani.paredes@newtrisko.de", createAt:'2022-12-11'},
-    {id:2, nombre:"Coco", apellido:"Paredes Valera", email:"Coco.paredes@newtrisko.de", createAt:'2022-12-11'},
-    {id:3, nombre:"Mika", apellido:"Paredes Valera", email:"Mika.paredes@newtrisko.de", createAt:'2022-12-11'},
-    {id:4, nombre:"Paolo", apellido:"Paredes Valera", email:"Paolo.paredes@newtrisko.de", createAt:'2022-12-11'},
-    {id:5, nombre:"Jasp", apellido:"Paredes Valera", email:"Jasp.paredes@newtrisko.de", createAt:'2022-12-11'},
-    {id:6, nombre:"Fede", apellido:"Paredes Valera", email:"Fede.paredes@newtrisko.de", createAt:'2022-12-11'},
-    {id:7, nombre:"Mark", apellido:"Paredes Valera", email:"Mark.paredes@newtrisko.de", createAt:'2022-12-11'},
-    {id:8, nombre:"Alex", apellido:"Paredes Valera", email:"Alex.paredes@newtrisko.de", createAt:'2022-12-11'},
-    {id:9, nombre:"Fran", apellido:"Paredes Valera", email:"Fran.paredes@newtrisko.de", createAt:'2022-12-11'},
-    {id:10, nombre:"Eric", apellido:"Paredes Valera", email:"Eric.paredes@newtrisko.de", createAt:'2022-12-11'},
-    {id:11, nombre:"Rosa", apellido:"Paredes Valera", email:"Rosa.paredes@newtrisko.de", createAt:'2022-12-11'}
-  ]
+  //atributos
+  clientes: Cliente[];
 
-  constructor() { }
+  constructor(private clienteService: ClienteService) { }
 
   ngOnInit(): void {
+    this.clienteService.getClientes().subscribe(
+      cliente => this.clientes = cliente
+    );
   }
 
 }
