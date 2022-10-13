f620585c149151d0f564eb9582d6145401800c15
estefaniNewtrisko <estefani.paredes@newtrisko.de>
2022-10-13 13:20:39 +0200

    formComponent

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Cliente } from './cliente';
 import { CLIENTES } from "./clientes.json";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { map } from "rxjs/operators";
 
 @Injectable({
@@ -12,13 +12,23 @@ export class ClienteService {
 
   private urlEndpoint: string = 'http://localhost:8080/api/clientes';
 
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    //return of(CLIENTES); //convert Observable
     return this.http.get<Cliente[]>(this.urlEndpoint);
     /*return this.http.get(this.urlEndpoint).pipe(
       map(response => response as Cliente[])
     );*/
   }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndpoint, cliente, { headers: this.httpHeaders });
+  }
+
+  getCliente(id): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.urlEndpoint}/${id}`)
+  }
+
 }
---
+++ src/app/clientes/form.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+
+  public cliente: Cliente = new Cliente();
+  public titulo: string = "Crear Cliente";
+
+  constructor(private clientService: ClienteService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+  }
+
+  cargarCliente(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id']
+      if(id){// si existe
+        this.clientService.getCliente(id).subscribe((cliente) => this.cliente = cliente)
+      }
+    })
+  }
+
+  create(): void {
+    this.clientService.create(this.cliente)
+      .subscribe(cliente => {
+        this.router.navigate(['/clientes'])
+        Swal.fire('Nuevo cliente', `Cliente ${cliente.nombre} creado con éxito`, `success`)
+      }
+      )
+  }
+
+
+
+}
